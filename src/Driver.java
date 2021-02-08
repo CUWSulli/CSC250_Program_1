4f6641b84a9d9be740028acbdc97bad65346ec91
CUWSulli <78753434+CUWSulli@users.noreply.github.com>
2021-02-08 11:55:45 -0600

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,32 @@
 
-public class Driver {
+public class DriverforHW 
+{
+	public static void main(String[] args)
+	{
+		VowelCheck("Matthew");
+		// is this even working
+	}
+
+	public static void VowelCheck(String s)
+	{
+		char[] vowellist = {'a', 'e', 'i', 'o', 'u'};
+		for(int i = 0; i < s.length(); i++)
+		{
+			char check = s.charAt(i);
+			for (int e = 0; e < vowellist.length(); e++)
+			{
+				if (vowellist[e] == check)
+				{
+					System.out.println("Found"+vowellist[e]+"Vowels!");
+				}
+			}
+		}
+			
+		
+		
+		
+	}
+	
+
 
 }
